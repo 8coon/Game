3dbb928a0fad51c3f0625c75234fb5366f01f9ba
8coon <8coon@users.noreply.github.com>
2016-10-09 22:11:41 +0300

    Добавил скрипты по умолчанию

---
+++ Scripts/skin.lua
@@ -0,0 +1,55 @@
+
+skin_loadTexture("Art/Skin/dummy.png")
+skin_loadFont("Art/Skin/pexico.sc")
+
+skin_setWidgetTexture("FRAME", { l = 0, t = 0, h = 48, w = 48, left = 16, top = 16 })
+skin_setWidgetTexture("BUTTON", { l = 48, t = 0, h = 32, w = 64, left = 10, top = 10 })
+skin_setWidgetTexture("BUTTON_HOVER", { l = 112, t = 0, h = 32, w = 64, left = 10, top = 10 })
+skin_setWidgetTexture("BUTTON_PRESSED", { l = 176, t = 0, h = 32, w = 64, left = 10, top = 10 })
+skin_setWidgetTexture("BUTTON_GRAYED", { l = 240, t = 0, h = 32, w = 64, left = 10, top = 10 })
+
+
+skin_setWidgetTexture("SCROLLBOX_VR_FRAME", { l = 336, t = 17, h = 12, w = 8, left = 3, top = 3 })
+skin_setWidgetTexture("SCROLLBOX_HR_FRAME", { l = 344, t = 18, h = 8, w = 12, left = 3, top = 3 })
+
+skin_setWidgetTexture("SCROLLBOX_VR_UP", { l = 304, t = 0, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_VR_UP_HOVER", { l = 312, t = 0, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_VR_UP_PRESSED", { l = 320, t = 0, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_VR_UP_GRAYED", { l = 328, t = 0, h = 8, w = 8, left = 1, top = 1 })
+
+skin_setWidgetTexture("SCROLLBOX_VR_DOWN", { l = 304, t = 8, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_VR_DOWN_HOVER", { l = 312, t = 8, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_VR_DOWN_PRESSED", { l = 320, t = 8, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_VR_DOWN_GRAYED", { l = 328, t = 8, h = 8, w = 8, left = 1, top = 1 })
+
+skin_setWidgetTexture("SCROLLBOX_HR_UP", { l = 304, t = 16, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_UP_HOVER", { l = 312, t = 16, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_UP_PRESSED", { l = 320, t = 16, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_UP_GRAYED", { l = 328, t = 16, h = 8, w = 8, left = 1, top = 1 })
+
+skin_setWidgetTexture("SCROLLBOX_HR_DOWN", { l = 304, t = 24, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_DOWN_HOVER", { l = 312, t = 24, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_DOWN_PRESSED", { l = 320, t = 24, h = 8, w = 8, left = 1, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_DOWN_GRAYED", { l = 328, t = 24, h = 8, w = 8, left = 1, top = 1 })
+
+skin_setWidgetTexture("SCROLLBOX_VR", { l = 336, t = 0, h = 18, w = 8, left = 1, top = 5 })
+skin_setWidgetTexture("SCROLLBOX_VR_HOVER", { l = 344, t = 0, h = 18, w = 8, left = 1, top = 5 })
+skin_setWidgetTexture("SCROLLBOX_VR_PRESSED", { l = 352, t = 0, h = 18, w = 8, left = 1, top = 5 })
+skin_setWidgetTexture("SCROLLBOX_VR_GRAYED", { l = 344, t = 0, h = 18, w = 8, left = 1, top = 5 })
+
+skin_setWidgetTexture("SCROLLBOX_HR", { l = 360, t = 0, h = 8, w = 18, left = 5, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_HOVER", { l = 360, t = 8, h = 8, w = 18, left = 5, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_PRESSED", { l = 360, t = 16, h = 8, w = 18, left = 5, top = 1 })
+skin_setWidgetTexture("SCROLLBOX_HR_GRAYED", { l = 360, t = 8, h = 8, w = 18, left = 5, top = 1 })
+
+
+skin_setWidgetTexture("LIST", { l = 0, t = 54, h = 4, w = 4, left = 1, top = 1 })
+
+skin_setWidgetTexture("LIST_ITEM", { l = 0, t = 50, h = 4, w = 4, left = 1, top = 1 })
+skin_setWidgetTexture("LIST_ITEM_SELECTED", { l = 4, t = 50, h = 4, w = 4, left = 1, top = 1 })
+
+
+
+
+
+
---
+++ Scripts/system.lua
@@ -0,0 +1,20 @@
+function skin_loadTexture(file)
+    return internalCall("skin_loadTexture", file)
+end
+
+
+function skin_loadFont(file)
+    return internalCall("skin_loadFont", file)
+end
+
+
+function skin_setWidgetTexture(name, markup)
+    return internalCall("skin_setWidgetTexture", name, markup.l, markup.t, markup.h,
+            markup.w, markup.left, markup.top)
+end
+
+
+function font_setLetter(letter, x, y, width, height)
+    return internalCall("font_setLetter", letter, x, y, width, height)
+end
+
